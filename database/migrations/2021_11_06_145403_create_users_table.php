be841d92c06ac44954c133ffb245cf6ab43c8b3b
Jonny Koulouris <jonny.k@touchrightsoftware.com>
2021-11-06 19:16:36 +0000

    include api_key column when creating users table

--- database/migrations/2021_11_06_145403_create_users_table.php
+++ database/migrations/2021_11_06_145403_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('email');
             $table->string('password');
+            $table->string('api_key');
             $table->timestamps();
         });
     }
